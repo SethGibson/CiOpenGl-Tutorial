3f0bf3e351519edf0d6661844130cd2b8a334875
Seth Gibson <seth.gibson1@gmail.com>
2015-02-17 14:59:41 -0800

    fixing bad cinder resource path -sG

--- LessonInstancing/src/LessonInstancingApp.cpp
+++ LessonInstancing/src/LessonInstancingApp.cpp
@@ -1,21 +1,56 @@
 #include "cinder/app/AppNative.h"
 #include "cinder/app/RendererGl.h"
+#include "cinder/GeomIo.h"
+#include "cinder/gl/Batch.h"
 #include "cinder/gl/gl.h"
+#include "cinder/gl/GlslProg.h"
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
-class LessonInstancingApp : public AppNative {
-  public:
+class LessonInstancingApp : public AppNative
+{
+public:
 	void setup() override;
 	void mouseDown( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
+
+	struct CioParticle
+	{
+		vec3 PPosition;
+		vec4 PColor;
+		float PSize;
+	};
+
+	struct CioIVertex
+	{
+		vec3 VPosition;
+		vec2 VUV;
+	};
+
+private:
+	gl::VboRef mParticleData, mInstanceData;
+	geom::BufferLayout mParticleLayout, mInstanceLayout;
+	gl::BatchRef mParticleDraw;
+	gl::GlslProgRef mParticleShader;
+
 };
 
 void LessonInstancingApp::setup()
 {
+	vector<CioParticle> cParticles;
+	vector<CioIVertex> cInstVerts;
+
+	mParticleData = gl::Vbo::create(GL_ARRAY_BUFFER, cParticles, GL_STATIC_DRAW);
+	mParticleLayout.append(geom::Attrib::CUSTOM_0, 3, sizeof(CioParticle), offsetof(CioParticle, PPosition));
+	mParticleLayout.append(geom::Attrib::CUSTOM_1, 4, sizeof(CioParticle), offsetof(CioParticle, PColor));
+	mParticleLayout.append(geom::Attrib::CUSTOM_2, 1, sizeof(CioParticle), offsetof(CioParticle, PSize));
+
+	mInstanceData = gl::Vbo::create(GL_ARRAY_BUFFER, cParticles, GL_STATIC_DRAW);
+	//mInstanceLayout.append(geom::Attrib::POSITION)
+	//mInstanceLayout.append(geom::Attrib::TEX_COORD_0);
 }
 
 void LessonInstancingApp::mouseDown( MouseEvent event )
